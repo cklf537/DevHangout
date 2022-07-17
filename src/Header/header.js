201327389bdb8c5d2d4e9da930d213fc931720b4
cklf537 <chandu537@gmail.com>
2022-07-17 11:59:09 +0530

    home page edits

--- src/Header/header.js
+++ src/Header/header.js
@@ -3,9 +3,22 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import './header.css'
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import Item from '@mui/material/Grid'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const theme = createTheme();
+
+theme.typography.h3 = {
+    fontSize: '1.2rem',
+    '@media (min-width:600px)': {
+      fontSize: '1.5rem',
+    },
+    [theme.breakpoints.up('md')]: {
+      fontSize: '4.4rem',
+    },
+  };
+  
 
 export default function MainContainer() {
     return (
@@ -14,21 +27,26 @@ export default function MainContainer() {
             <Container maxWidth="false" className="mainContainer">
                 <Grid container>
                     <Grid item xs={12}  >
-                        <Box sx={{ bgcolor: '#cfe8fc', height: '50vh', padding: '15px' }}>Chandra</Box>
+                        <Box sx={{ bgcolor: '#cfe8fc', height: '50vh', padding: '15px' }}>
+                        <ThemeProvider theme={theme}>
+                        <Typography variant="h3"> CHANDRASHEKAR BOGGAVARPU</Typography>
+                        <Typography variant='p'>Fullstack developer</Typography>
+                        </ThemeProvider>
+                        </Box>
                     </Grid>
                     <Grid item xs={12}  >
                         <Grid container spacing={2}>
                             <Grid item xs={6} md={8}>
-                                <Item>xs=6 md=8</Item>
+                                <Item></Item>
                             </Grid>
                             <Grid item xs={6} md={4}>
-                                <Item>xs=6 md=4</Item>
+                                <Item></Item>
                             </Grid>
                             <Grid item xs={6} md={4}>
-                                <Item>xs=6 md=4</Item>
+                                <Item></Item>
                             </Grid>
                             <Grid item xs={6} md={8}>
-                                <Item>xs=6 md=8</Item>
+                                <Item></Item>
                             </Grid>
                         </Grid>
                     </Grid>
