80262f7a4f907ce3b14838070bb7b7b487f21822
cklf537 <chandu537@gmail.com>
2022-07-17 12:12:32 +0530

    landing page changes

--- src/App.js
+++ src/App.js
@@ -2,25 +2,9 @@ import React, {Component} from "react";
 import MainContainer from "../src/Header/header"
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state={
-      name: "Developer"
-    }
-  }
-
-  changeName = ()=>{
-    this.setState({name : 'Developers Hangout!!!'});
-  }
-
   render(){
     return (
       <MainContainer />
-
-      // <div className="App">
-      //  `welcome {this.state.name} !`
-      //  <button onClick={this.changeName}>Click!</button>
-      // </div>
     );
   }
 }
